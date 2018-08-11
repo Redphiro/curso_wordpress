654719568bd725a5a453f3fc4325b856a92259f2
TriForceX <triforce@gznetwork.com>
2018-08-11 17:00:54 -0400

    activacion template

---
+++ wp-content/themes/cursowptheme/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Documento sin título</title>
+	
+	
+	<?php
+	/* Always have wp_head() just before the closing </head>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to add elements to <head> such
+	 * as styles, scripts, and meta tags.
+	 */
+	wp_head();
+	?>
+</head>
+<body>
+	
+	<p>Header</p>
---
+++ wp-content/themes/cursowptheme/index.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+
+
+
+<p>Hola mundo</p>
+
+
+
+
+<?php get_footer(); ?>
